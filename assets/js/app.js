4f10c024dcb6b3275311375d4a758d2698c1ca83
Rogers Teixeira <rogerguedes.ft@gmail.com>
2025-12-08 01:01:08 -0300

    navbars okay

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,10 +2,40 @@ window.onload = (event) => {
     console.log("page is fully loaded");
     //FIXME remove after dev
     initialTitle = document.title;
-    setInterval(function(){
+    setInterval(function () {
         currWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
         currHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
         //document.title = initialTitle + " [" + currWidth + "x" + currHeight + "]";
         document.title = currWidth + "x" + currHeight;
     }, 1000)
+
+    // Source - https://stackoverflow.com/a
+    // Posted by joshmoto, modified by community. See post 'Timeline' for change history
+    // Retrieved 2025-12-08, License - CC BY-SA 4.0
+
+    // // scroll functions
+    // $(window).scroll(function (e) {
+    //     // add/remove class to navbar when scrolling to hide/show
+    //     $('.navbar')[$(window).scrollTop() >= 150 ? 'addClass' : 'removeClass']('navbar-hide');
+    // });
+    var lastScrollTop = 0;
+    $(window).scroll(function (e) {
+        console.log("scroll");
+        // // add/remove class to navbar when scrolling to hide/show
+        // var scroll = $(window).scrollTop();
+        // if (scroll >= 150) {
+        //     $('.navbar').addClass("navbar-hide");
+        // } else {
+        //     $('.navbar').removeClass("navbar-hide");
+        // }
+        var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+        if (st > lastScrollTop) {
+            // downscroll code
+            $('#mainNavbar').addClass("navbar-hide");
+        } else if (st < lastScrollTop) {
+            // upscroll code
+            $('#mainNavbar').removeClass("navbar-hide");
+        } // else was horizontal scroll
+        lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+    });
 };
