c116882f29f02c773f89095c53589b9f2cdce2cf
Rogers Teixeira <rogerguedes.ft@gmail.com>
2025-12-09 23:26:32 -0300

    done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,27 @@
+function width2breakpointName(num){
+    if (num < 576) {
+        return "";
+    } else if (num < 768) {
+        return "sm";
+    } else if (num < 992) {
+        return "md";
+    } else if (num < 1200) {
+        return "lg";
+    } else {
+        return "xl";
+    }
+}
+
 window.onload = (event) => {
-    console.log("page is fully loaded");
-    //FIXME remove after dev
-    initialTitle = document.title;
-    setInterval(function () {
-        currWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-        currHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-        //document.title = initialTitle + " [" + currWidth + "x" + currHeight + "]";
-        document.title = currWidth + "x" + currHeight;
-    }, 1000)
+    // console.log("page is fully loaded");
+    // //FIXME comment after dev
+    // initialTitle = document.title;
+    // setInterval(function () {
+    //     currWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    //     currHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    //     //document.title = initialTitle + " [" + currWidth + "x" + currHeight + "]";
+    //     document.title = currWidth + "x" + currHeight + ": " + width2breakpointName(currWidth);
+    // }, 1000)
 
     // Source - https://stackoverflow.com/a
     // Posted by joshmoto, modified by community. See post 'Timeline' for change history
@@ -20,7 +34,7 @@ window.onload = (event) => {
     // });
     var lastScrollTop = 0;
     $(window).scroll(function (e) {
-        console.log("scroll");
+        // console.log("scroll");
         // // add/remove class to navbar when scrolling to hide/show
         // var scroll = $(window).scrollTop();
         // if (scroll >= 150) {
